f8a04c4aca0393d16a3b961f8e1f9c20c73879d6
maYunLaoXi <593871583@qq.com>
2021-06-03 18:53:11 +0800

    remove: 去掉灰色

--- docs/.vuepress/public/filter-css.js
+++ docs/.vuepress/public/filter-css.js
@@ -1,14 +1,15 @@
 (function(){
-  setTimeout(() => {
-    var three = 3 * 24 * 60 * 60 * 1000
-    var now = +new Date()
-    var end = +new Date('2021-05-26 23:00:00')
-    if (end - now > 0) {
-      document.body.style.filter = 'grayscale(1)'
-    }
-    setTimeout(() => {
-      var siteName = document.querySelector('.site-name')
-      siteName.innerText = siteName.innerText + '（缅怀双星）'
-    }, 1000);
-  }, 100)
+  console.log('design by maYunLaoXi')
+  // setTimeout(() => {
+  //   var three = 3 * 24 * 60 * 60 * 1000
+  //   var now = +new Date()
+  //   var end = +new Date('2021-05-26 23:00:00')
+  //   if (end - now > 0) {
+  //     document.body.style.filter = 'grayscale(1)'
+  //     setTimeout(() => {
+  //       var siteName = document.querySelector('.site-name')
+  //       siteName.innerText = siteName.innerText + '（缅怀双星）'
+  //     }, 1000);
+  //   }
+  // }, 100)
 })()
